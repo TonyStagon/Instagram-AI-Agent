91178ff49fc81e79b0e8e5458e4346927deabab5
Tony Stagon <leelethabi@gmail.com>
2025-05-09 18:02:32 +0200

    Schema fix

--- src/secret/index.ts
+++ src/secret/index.ts
@@ -7,10 +7,10 @@ export const Xusername: string = process.env.Xusername || "default_Xusername";
 export const Xpassword: string = process.env.Xpassword || "default_Xpassword";
 
 export const TWITTER_API_CREDENTIALS = {
-  appKey: process.env.TWITTER_API_KEY || "default_TWITTER_API_KEY",
-  appSecret: process.env.TWITTER_API_SECRET || "default_TWITTER_API_SECRET",
-  accessToken: process.env.TWITTER_ACCESS_TOKEN || "default TWITTER_ACCESS_TOKEN",
-  accessTokenSecret: process.env.TWITTER_ACCESS_SECRET || "default_TWITTER_ACCESS_SECRET",
+  consumerKey: process.env.TWITTER_API_KEY || "default_TWITTER_API_KEY",
+  consumerSecret: process.env.TWITTER_API_KEY_SECRET || "default_TWITTER_API_SECRET",
+  accessToken: process.env.TWITTER_ACCESS_TOKEN || "default_TWITTER_ACCESS_TOKEN",
+  accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET || "default_TWITTER_ACCESS_SECRET",
   bearerToken: process.env.TWITTER_BEARER_TOKEN || "default_TWITTER_BEARER_TOKEN",
 }
 
